b1220060e1672c2896256ee800368dec903e9b96
Arthur Rivera <arturorivera7700@gmail.com>
2025-02-13 02:28:21 +0000

    feat: guards andando

--- example/lib/app/router/router.dart
+++ example/lib/app/router/router.dart
@@ -1,10 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:guard_example/app/router/router.gr.dart';
-import 'package:guard_example/bootstrap.dart';
+import 'package:guard_example/guards/example_guard.dart';
 import 'package:guards/auto_route.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
+
+  AppRouter(this.exampleGuards);
+
+  final ExampleGuards exampleGuards;
+
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: HomeRoute.page, initial: true),
@@ -13,6 +18,7 @@ class AppRouter extends RootStackRouter {
           path: '/login',
           children: [
             AutoRoute(page: SignInRoute.page, path: 'signin'),
+            AutoRoute(page: SignoutRoute.page, path: 'signout'),
           ],
         ),
         AutoRoute(
@@ -24,6 +30,7 @@ class AppRouter extends RootStackRouter {
         ),
         AutoRoute(
           page: RestrictedRoute.page,
+          path: '/restricted',
           guards: [
             exampleGuards.login.toAutoRouteGuard(),
             exampleGuards.camera.toAutoRouteGuard(),
