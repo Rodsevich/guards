4fcb6f50977b2a5b3355a82c2a48c122b7a46e24
Lucas Tobon <lucas.tobon.v@gmail.com>
2025-02-25 18:26:26 -0500

    Se retira base guard de lib

--- lib/src/common_guards/base.dart
+++
@@ -1,7 +0,0 @@
-import 'package:guards/guards.dart';
-
-/// A guard used to demonstrate documentation for a public member.
-abstract base class ExampleGuard extends GuardBase {
-  /// Constructs a new instance of [ExampleGuard].
-  ExampleGuard({super.persistenceDelegate});
-}
