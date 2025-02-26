a3571536a97dcd19c7b06ef767010332001dd1e8
Lucas Tobon <lucas.tobon.v@gmail.com>
2025-02-26 07:07:45 -0500

    Correcciones en CameraGuard

--- lib/src/common_guards/camera_guard.dart
+++ lib/src/common_guards/camera_guard.dart
@@ -11,11 +11,15 @@ import 'package:universal_html/html.dart' as html;
 final class CameraGuard extends GuardBase {
   /// Creates a [CameraGuard] instance.
   ///
-  /// The [cameraPermissionPath] is used for redirecting 
+  /// The [cameraPermissionPath] is used for redirecting
   /// when the camera permission is not granted.
   /// {@macro camera_guard}
-  CameraGuard({required this.cameraPermissionPath})
-    : super(persistenceDelegate: CameraPersistenceDelegate());
+  CameraGuard({
+    required this.cameraPermissionPath,
+    CameraPersistenceDelegate? persistenceDelegate,
+  }) : super(
+            persistenceDelegate:
+                persistenceDelegate ?? CameraPersistenceDelegate(),);
 
   /// The path to redirect if the camera permission is not granted.
   final String cameraPermissionPath;
@@ -26,12 +30,13 @@ final class CameraGuard extends GuardBase {
   @override
   String get redirectPath => cameraPermissionPath;
 
-  /// Checks camera permission, persists it, and 
+  /// Checks camera permission, persists it, and
   /// returns whether permission is granted.
   @override
   Future<bool> setUpAndInitializeGuard() async {
     final hasPermission = await checkCameraPermission();
-    await persistenceDelegate!.updateGuardStatus(guardIdentifier, hasPermission);
+    await persistenceDelegate?.updateGuardStatus(
+        guardIdentifier, hasPermission,);
     return hasPermission;
   }
 }
@@ -48,11 +53,6 @@ Future<bool> checkCameraPermission() async {
     }
   } else {
     final status = await Permission.camera.status;
-    if (status.isGranted) {
-      return true;
-    } else {
-      final result = await Permission.camera.request();
-      return result.isGranted;
-    }
+    return status.isGranted;
   }
 }
